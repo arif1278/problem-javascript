fc7b5a0a5f6fe67880e34d2a2a51c6fcda065630
arifhossain <manabethreeplusone@gmail.com>
2022-08-09 15:57:23 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,25 @@
+const country = 'Bangladesh';
+const age = 52;
+const isIndependent = true;
+const student = { id: 121, class: 11, name: 'Agun' };
+const friends = [13, 14, 15, 11, 15, 17, 20, 19];
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(Array.isArray(friends));
+console.log(typeof add);
+
+console.log(friends.includes(19));
+console.log(friends.includes(21));
+if (friends.indexOf(19) !== -1) {
+
+}
+const newFriendsAge = [12, 13, 11, 13]
+const allFriends = newFriendsAge.concat(friends);
+console.log(allFriends)
+
---
+++ slice.js
@@ -0,0 +1,13 @@
+const friends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
+
+// remove elements from an array
+// and,if necessary,inserts new elements in their place,
+// returning the deleted elements.
+// will change the original array
+
+// const partial = friends.slice(2, 5);
+// console.log(partial);
+// console.log(friends);
+
+const partial = friends.splice(2, 5);
+console.log(partial);
